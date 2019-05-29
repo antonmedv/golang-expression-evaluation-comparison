a913c3089bcbadc6c816a1f71e7c2b915f37b8cf
Anton Medvedev <anton@medv.io>
2019-05-29 15:58:05 +0700

    Add startswith bench

--- celgo_test.go
+++ celgo_test.go
@@ -49,3 +49,46 @@ func Benchmark_celgo(b *testing.B) {
 		b.Fail()
 	}
 }
+
+func Benchmark_celgo_startswith(b *testing.B) {
+	params := map[string]interface{}{
+		"name":  "/groups/foo/bar",
+		"group": "foo",
+	}
+
+	env, err := cel.NewEnv(
+		cel.Declarations(
+			decls.NewIdent("name", decls.String, nil),
+			decls.NewIdent("group", decls.String, nil),
+		),
+	)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	parsed, issues := env.Parse(`name.startsWith("/groups/" + group)`)
+	if issues != nil && issues.Err() != nil {
+		b.Fatalf("parse error: %s", issues.Err())
+	}
+	checked, issues := env.Check(parsed)
+	if issues != nil && issues.Err() != nil {
+		b.Fatalf("type-check error: %s", issues.Err())
+	}
+	prg, err := env.Program(checked)
+	if err != nil {
+		b.Fatalf("program construction error: %s", err)
+	}
+
+	var out ref.Val
+
+	for n := 0; n < b.N; n++ {
+		out, _, err = prg.Eval(params)
+	}
+
+	if err != nil {
+		b.Fatal(err)
+	}
+	if !out.Value().(bool) {
+		b.Fail()
+	}
+}
--- expr_test.go
+++ expr_test.go
@@ -27,3 +27,28 @@ func Benchmark_expr(b *testing.B) {
 		b.Fail()
 	}
 }
+
+func Benchmark_expr_startswith(b *testing.B) {
+	params := map[string]interface{}{
+		"name":  "/groups/foo/bar",
+		"group": "foo",
+	}
+
+	program, err := expr.Compile(`name startsWith "/groups/" + "foo"`, expr.Env(params))
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	var out interface{}
+
+	for n := 0; n < b.N; n++ {
+		out, err = expr.Run(program, params)
+	}
+
+	if err != nil {
+		b.Fatal(err)
+	}
+	if !out.(bool) {
+		b.Fail()
+	}
+}
